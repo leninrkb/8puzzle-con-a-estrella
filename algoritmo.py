e22389ea8bbfd40a8b3487cc2ee48a0cd868521f
Lenin Acosta <rkb.lenin@gmail.com>
2023-04-15 14:37:50 -0500

    terminado

--- algoritmo.py
+++ algoritmo.py
@@ -1,27 +1,60 @@
 import copy
 import numpy as np
+import logging
+# from numba import njit
 
+logging.basicConfig(filename='resultado.log', level=logging.INFO, filemode='w', format='%(levelname)s:%(name)s:\n%(message)s')
 global TABLERO
-TABLERO = np.array([
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-])
+TABLERO = np.array(
+    [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 0],
+    ]
+)
 
-def calcular_euristica(tablero_nodo, tablero_solucion=TABLERO):
+def calcular_euristica(tablero, tablero_solucion=TABLERO):
+    listanormal = tablero.flatten().tolist()
+    listasolucion = tablero_solucion.flatten().tolist()
     euristica = 0
-    x, y = tablero_solucion.shape
-    for i in range(x):
-        for j in range(y):
-            if not tablero_nodo[i, j] == tablero_solucion[i, j]:
-                euristica+=1
+    for index in range(len(listanormal)):
+        if not listanormal[index] == listasolucion[index]:
+            euristica+=1
+        # indicesolucion = listasolucion.index(listanormal[index])
+        # euristica+= abs(index-indicesolucion)
     return euristica
 
+def es_solucion(lista_nodos):
+    for nodo in lista_nodos:
+        if nodo.hn == 0:
+            imprimir_camino(nodo)
+            return True
+    return False
+
 def nodo_por_expandir(lista_prioridad):
-    fn = []
-    for i in range(len(lista_prioridad)):
-        fn.append(lista_prioridad[i].gn + lista_prioridad[i].hn)
-    return min(fn)
+    fns = []
+    for index, nodo in enumerate(lista_prioridad):
+        if nodo.estado == 'expandido':
+            continue
+        fns.append((index, nodo.hn, nodo))
+    item = min(fns, key=lambda x: x[1])
+    lista_prioridad.pop(item[0])
+    nodo = item[2]
+    nodo.estado = 'expandido'
+    return nodo
+
+def agregar_nodos(lista_prioridad, nuevos_nodos):
+    for nodo in nuevos_nodos:
+        # if controlar_repetido(lista_prioridad, nodo):
+        #     continue
+        lista_prioridad.append(nodo)
+
+def controlar_repetido(lista_prioridad, nodo):
+    for index, nodo in enumerate(lista_prioridad):
+        nodot = lista_prioridad[index].tablero
+        if np.array_equal(nodo, nodot):
+            return True
+    return False
 
 def generar_estados(nodo):
     nodo_arriba = copy.deepcopy(nodo)
@@ -30,32 +63,53 @@ def generar_estados(nodo):
     nodo_derecha = copy.deepcopy(nodo)
     nodos_generados = []
     if nodo_arriba.mover_arriba():
-        nodo_arriba.accion = 'movio arriba'
-        nodo_arriba.gn+=1
+        nodo_arriba.accion = "movio arriba"
+        nodo_arriba.estado = ''
+        nodo_arriba.gn += 1
         nodo_arriba.hn = calcular_euristica(nodo_arriba.tablero)
         nodo_arriba.calcfn()
+        nodo_arriba.padre = nodo
         nodos_generados.append(nodo_arriba)
     if nodo_abajo.mover_abajo():
-        nodo_abajo.accion = 'movio abajo'
-        nodo_abajo.gn+=1
+        nodo_abajo.accion = "movio abajo"
+        nodo_abajo.estado = ''
+        nodo_abajo.gn += 1
         nodo_abajo.hn = calcular_euristica(nodo_abajo.tablero)
         nodo_abajo.calcfn()
+        nodo_abajo.padre = nodo
         nodos_generados.append(nodo_abajo)
     if nodo_izquierda.mover_izquierda():
-        nodo_izquierda.accion = 'movio izquierda'
-        nodo_izquierda.gn+=1
+        nodo_izquierda.accion = "movio izquierda"
+        nodo_izquierda.estado = ''
+        nodo_izquierda.gn += 1
         nodo_izquierda.hn = calcular_euristica(nodo_izquierda.tablero)
         nodo_izquierda.calcfn()
+        nodo_izquierda.padre = nodo
         nodos_generados.append(nodo_izquierda)
     if nodo_derecha.mover_derecha():
-        nodo_derecha.accion = 'movio derecha'
-        nodo_derecha.gn+=1
+        nodo_derecha.accion = "movio derecha"
+        nodo_derecha.estado = ''
+        nodo_derecha.gn += 1
         nodo_derecha.hn = calcular_euristica(nodo_derecha.tablero)
         nodo_derecha.calcfn()
+        nodo_derecha.padre = nodo
         nodos_generados.append(nodo_derecha)
     if len(nodos_generados) == 0:
         return None
     return nodos_generados
 
 
+def es_solucionable(tablero):
+    # Convertir la lista del estado inicial en una cadena
+    estado_str = "".join(str(x) for x in tablero)
+    # Contar el número de inversiones en la cadena
+    inversiones = sum( 1 for i in range(8) for j in range(i + 1, 9) if estado_str[i] > estado_str[j] and estado_str[i] != "0" )
+    # Comprobar si el número de inversiones es par o impar
+    return inversiones % 2 == 0
 
+def imprimir_camino(nodo):
+    if nodo == None:
+        return
+    # print(f'{nodo.tablero} \n')
+    logging.info(nodo.tablero)
+    return imprimir_camino(nodo.padre)
--- nodo.py
+++ nodo.py
@@ -1,18 +1,54 @@
 import numpy as np
+from random import shuffle
+
 class Nodo:
     def __init__(self):
         self.tablero = np.array([
-            [7, 1, 8],
-            [6, 3, 0],
-            [2, 5, 4],
+             [2, 3, 6],
+            [0, 4, 8],
+            [1, 7, 5]
         ])
         self.padre = None
         self.gn = 0 #costo camino
         self.hn = 0 #euristica
         self.fn = 0 #costo total
         self.accion = 'tablero inicial'
+        self.estado = ''
+
     def calcfn(self):
         self.fn = self.gn + self.hn
+
+    def facil(self):
+        self.tablero = np.array([
+            [4, 1, 3],
+            [2, 8, 5],
+            [0, 7, 6],
+        ])
+    def medio(self):
+        self.tablero = np.array([
+            [4, 8, 1],
+            [2, 6, 3],
+            [7, 5, 0],
+        ])
+    def dificil(self):
+        self.tablero = np.array([
+            [2, 4, 1],
+            [8, 3, 0],
+            [7, 6, 5],
+        ])
+    def llamen_a_dios(self):
+        self.tablero = np.array([
+            [1, 5, 6],
+            [4, 8, 2],
+            [7, 0, 3],
+        ])
+
+    def mezclar_tablero(self):
+        flat_puzzle = self.tablero.flatten().tolist()
+        shuffle(flat_puzzle)
+        shuffled_puzzle = np.array(flat_puzzle).reshape(self.tablero.shape)
+        self.tablero = shuffled_puzzle
+
     def poscicion_cero(self):
         x, y = self.tablero.shape
         for i in range(x):
@@ -43,5 +79,7 @@ class Nodo:
             self.tablero[x, y], self.tablero[x, y+1] = self.tablero[x, y+1], self.tablero[x, y]
             return True
         return False
+    
     def imprimir_nodo(self):
         print(f'{self.tablero} - gn:{self.gn} - hn:{self.hn} - fn:{self.fn}\n accion: {self.accion} \n')
+    
